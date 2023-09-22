040ee9145b7eed4a8ad93184a4ff79cd0dc7016b
akumbom <akumbom5ma@gmail.com>
2023-09-22 14:59:18 +0100

    .

--- IdentityServer/ClientConfiguration.cs
+++ IdentityServer/ClientConfiguration.cs
@@ -30,32 +30,38 @@ namespace IdentityServer
 
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
                     // scopes that client has access to
-                    AllowedScopes = { "api1" }
+                    AllowedScopes = { "api", "api1" }
                 };
             }
         }
         // For interactive applications
+        //RedirectUris = { "https://localhost:5002/signin-oidc" },
+        //PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
         public static Client InteractiveClient
         {
             get
             {
                 return new Client
                 {
-                    ClientId = "mvc",
+                    ClientId = "spa",
+                    //ClientId = "mvc",
                     ClientSecrets = { new Secret("secret".Sha256()) },
 
                     AllowedGrantTypes = GrantTypes.Code,
 
                     // where to redirect to after login
-                    RedirectUris = { "https://localhost:5002/signin-oidc" },
+                    RedirectUris = { "http://localhost:4200/#/signin-oidc" },
 
                     // where to redirect to after logout
-                    PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+                    PostLogoutRedirectUris = { "http://localhost:4200/#/signout-callback-oidc" },
 
                     AllowedScopes = new List<string>
                     {
                         IdentityServerConstants.StandardScopes.OpenId,
-                        IdentityServerConstants.StandardScopes.Profile
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        IdentityServerConstants.StandardScopes.OfflineAccess,
+                        "api"
                     }
                 };
             }
